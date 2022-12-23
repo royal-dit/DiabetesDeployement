b8f4b2671b046439d526dff298dcfa8866e84c13
Royal dahal <royaldahal100@gmail.com>
2022-12-23 22:48:19 +0545

    changes

--- main.py
+++ main.py
@@ -53,5 +53,5 @@ def predict_banknote(data:Diabetes):
 # if __name__ =='__main__':
 #     uvicorn.run(app,host='127.0.0.1',port=8001)
 if __name__ =='__main__':
-    main.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0',port=8080)
 
